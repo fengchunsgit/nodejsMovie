0eedf0f388bee7e6f102c7b4011c7634a7a7ef9a
fengchunsgit <1055688803@qq.com>
2018-12-08 07:27:11 +0800

    Schemas and Model

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.get('/movie/:id',function(req,res){
         country:'美国',
         year:2014,
         poster:'http://img31.mtime.cn/pi/2013/12/13/112514.36500695_1000X1000.jpg',
-        flash:'http://112.25.17.171/6777B53ACBA33762CB4BC5323/030020010053F2F2D4A99F1903B9EDBFDE05C9-3649-F6C2-B592-18F27851DA7B.mp4?ccode=0502&duration=272&expire=18000&psid=7c8f60eaa7ecc69154870addfc8e0e91&ups_client_netip=b7c7257e&ups_ts=1544183921&ups_userid=&utid=iL65EFd3MjMCARu6pMNCkshw&vid=XNzU4MTIxNzI4&vkey=Aeb9de3a8a95be524af62a229cddd37e4&sp=',
+        flash:'http://player.youku.com/player.php/sid/XNjA1Njc0NTUy/v.swf',
         summary:'毒液金句: 埃迪，你的身体，你的一切都是我的！ 埃迪，我开始有点喜欢你了！ 埃迪你敢说我是寄生虫，道歉！快向我道歉！ 埃迪，我这么做都是因为你',
         language:'英语'
       }
@@ -80,7 +80,7 @@ app.get('/admin/list',function(req,res){
         country:"美国",
         year:2014,
         language:"英语",
-        flash:"https://www.bilibili.com/video/av35613192/"
+        flash:"http://player.youku.com/player.php/sid/XNjA1Njc0NTUy/v.swf"
       }]
     })
 })
