2f8e71e72d91e0438f3916105ce29b6d00ea4c5b
fengchunsgit <1055688803@qq.com>
2018-12-23 11:20:34 +0800

    配置入口文件

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app=express()
 var path=require('path')
 var bodyParser = require('body-parser')
 var mongoose=require('mongoose')
+var logger=require('morgan')
 
 DB_URL = 'mongodb://localhost:12345/imooc';
 var session = require('express-session'); //如果要使用session，需要单独包含这个模块
@@ -26,6 +27,15 @@ app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x
 app.use(express.static(path.join(__dirname,'public/')))
 app.use('/static',express.static('static'))
 app.locals.moment=require('moment')
+
+//310 配置开发环境
+
+if('development'===app.get('env')){
+  app.set('showStackError',true)
+  app.use(logger(':method :url :status'))
+  app.locals.pretty=true
+  mongoose.set('debug',true)
+}
 
 require('./config/routes')(app)
 const port=3000
